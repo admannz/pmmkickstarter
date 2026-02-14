01442c24d91527d1816269f815feef387cd8813d
Adam Hall (MSFT) <adamdvhall@gmail.com>
2026-02-14 14:18:27 -0800

    Update blog-data.js

--- blog-data.js
+++ blog-data.js
@@ -1,17 +1,24 @@
 // blog-data.js
 const blogPosts = [
-    {
-        id: 1,
-        title: "Why Your Positioning Isn't Working",
-        date: "October 10, 2024",
-        summary: "Most startups mistake a tagline for positioning. Here is the difference and how to fix it...",
-        link: "post-positioning-mistakes.html" 
+     {
+        id: 99,
+        title: "Using AI At Work",
+        date: "May 2025",
+        summary: "AI is changing productivity in product marketing, read about practical use cases for idea generation, research, and content transformation.",
+        link: "posts/Using-AI-at-work.html" 
     },
     {
         id: 2,
+        title: "Why Your Positioning Isn't Working",
+        date: "October 10, 2024",
+        summary: "Most startups mistake a tagline for positioning. Here is the difference and how to fix it...",
+        link: "posts/post-positioning-mistakes.html" 
+    },
+    {
+        id: 1,
         title: "The First 90 Days of a PMM",
         date: "September 22, 2024",
         summary: "What you should focus on when you are the first product marketer on the ground.",
-        link: "post-first-90-days.html"
+        link: "posts/post-first-90-days.html"
     }
 ];
