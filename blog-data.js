1ed83042f0327a12623ff92d053bfe61afafe162
Adam Hall (MSFT) <adamdvhall@gmail.com>
2026-02-14 15:16:27 -0800

    Final repost of old blogs

--- blog-data.js
+++ blog-data.js
@@ -8,17 +8,42 @@ const blogPosts = [
         link: "posts/Using-AI-at-work.html" 
     },
     {
-        id: 2,
+        id: 5,
         title: "Know Your Currency",
         date: "December 2024",
         summary: "A reminder to think about and understand your personal currency. What motivates you and what you want to get paid in.",
         link: "posts/Know-Your-Currency.html" 
     },
     {
-        id: 1,
+        id: 4,
         title: "Presentations and Demos",
         date: "January 2023",
         summary: "Do you need to do presentations or demos at work? Learn practical tips to overcome nerves, prepare effectively, and crush your delivery.",
         link: "posts/Presentations-And-Demos.html"
-    }
+    },
+    {
+      id: 3,
+        title: "Hooray For Generalists",
+        date: "January 2023",
+        summary: "A tribute to the generalists—those who thrive in multiple disciplines and provide unique perspectives across workstreams.",
+        link: "posts/Hooray-For-Generalists.html"  
+    },
+      {
+      id: 2,
+        title: "Surviving a Career Pivot",
+        date: "September 2022",
+        summary: "Explore four models for career pivots—from iteration to a full reset—and how to identify your transferable skills.",
+        link: "posts/Surviving-Career-Pivots.html"  
+      },
+      {
+      id: 1,
+        title: "PMM Fundamentals",
+        date: "August 2022",
+        summary: "Explore the core responsibilities of Product Marketing: influencing product strategy, creating market narratives, and orchestrating key workstreams.",
+        link: "posts/PMM-Fundamentals.html"  
+      }
+
+
+
+      Explore the core responsibilities of Product Marketing: influencing product strategy, creating market narratives, and orchestrating key workstreams.
 ];
